d6ec9bdc227c00c29d4d0142bf0b7e82263bc2a8
Daler <dsarifkulo021@gmail.com>
2025-04-23 15:56:38 +0500

    fixed

--- frontend/components/leadForm.tsx
+++ frontend/components/leadForm.tsx
@@ -42,7 +42,7 @@ const LeadForm: React.FC<leadFormProps> = () => {
 			const timeoutId = setTimeout(() => controller.abort(), 10000);
 
 			const res = await fetch(
-				process.env.NEXT_PUBLIC_BASE_URL + "/api/send-lead",
+				process.env.NEXT_PUBLIC_BASE_URL + "/send-lead",
 				{
 					method: "POST",
 					headers: { "Content-Type": "application/json" },
